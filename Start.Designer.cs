b538239dac1a1002e94455904abb65d7230c39d3
David <sohodf@gmail.com>
2013-10-10 14:54:26 +0200

    post GUI changes

--- Start.Designer.cs
+++ Start.Designer.cs
@@ -78,7 +78,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(18, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(127, 19);
             this.label1.TabIndex = 0;
@@ -88,17 +88,17 @@
             // 
             this.devicesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.devicesComboBox.FormattingEnabled = true;
-            this.devicesComboBox.Location = new System.Drawing.Point(145, 22);
+            this.devicesComboBox.Location = new System.Drawing.Point(156, 21);
             this.devicesComboBox.Name = "devicesComboBox";
-            this.devicesComboBox.Size = new System.Drawing.Size(236, 21);
+            this.devicesComboBox.Size = new System.Drawing.Size(212, 21);
             this.devicesComboBox.TabIndex = 1;
             this.devicesComboBox.SelectedValueChanged += new System.EventHandler(this.Devices_SelectedValueChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(530, 22);
+            this.button1.Location = new System.Drawing.Point(493, 22);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.Size = new System.Drawing.Size(124, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Refresh devices";
             this.button1.UseVisualStyleBackColor = true;
@@ -116,11 +116,12 @@
             // 
             // deviceStatusTextBox
             // 
-            this.deviceStatusTextBox.Location = new System.Drawing.Point(390, 22);
+            this.deviceStatusTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.deviceStatusTextBox.Location = new System.Drawing.Point(374, 21);
             this.deviceStatusTextBox.Multiline = true;
             this.deviceStatusTextBox.Name = "deviceStatusTextBox";
             this.deviceStatusTextBox.ReadOnly = true;
-            this.deviceStatusTextBox.Size = new System.Drawing.Size(134, 23);
+            this.deviceStatusTextBox.Size = new System.Drawing.Size(97, 21);
             this.deviceStatusTextBox.TabIndex = 6;
             this.deviceStatusTextBox.Text = "Device Status";
             this.deviceStatusTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -129,9 +130,9 @@
             // openApkBtn
             // 
             this.openApkBtn.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.openApkBtn.Location = new System.Drawing.Point(6, 19);
+            this.openApkBtn.Location = new System.Drawing.Point(7, 21);
             this.openApkBtn.Name = "openApkBtn";
-            this.openApkBtn.Size = new System.Drawing.Size(92, 40);
+            this.openApkBtn.Size = new System.Drawing.Size(116, 38);
             this.openApkBtn.TabIndex = 7;
             this.openApkBtn.Text = "Select apk file";
             this.openApkBtn.UseVisualStyleBackColor = true;
@@ -139,13 +140,14 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.BackColor = System.Drawing.Color.LightCyan;
             this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.install);
             this.groupBox1.Controls.Add(this.openApkBtn);
-            this.groupBox1.Location = new System.Drawing.Point(16, 127);
+            this.groupBox1.Controls.Add(this.install);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Location = new System.Drawing.Point(22, 127);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(233, 111);
+            this.groupBox1.Size = new System.Drawing.Size(596, 69);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "APK Management";
@@ -153,9 +155,9 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Arial", 7F);
-            this.button5.Location = new System.Drawing.Point(6, 63);
+            this.button5.Location = new System.Drawing.Point(468, 23);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(92, 39);
+            this.button5.Size = new System.Drawing.Size(116, 38);
             this.button5.TabIndex = 15;
             this.button5.Text = "Install as none sysapp";
             this.button5.UseVisualStyleBackColor = true;
@@ -164,9 +166,9 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button4.Location = new System.Drawing.Point(104, 63);
+            this.button4.Location = new System.Drawing.Point(313, 21);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(116, 39);
+            this.button4.Size = new System.Drawing.Size(116, 38);
             this.button4.TabIndex = 14;
             this.button4.Text = "Uninstall";
             this.ToolTip.SetToolTip(this.button4, "Uninstalls the wireless controller apk from the machine");
@@ -176,9 +178,9 @@
             // install
             // 
             this.install.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.install.Location = new System.Drawing.Point(104, 19);
+            this.install.Location = new System.Drawing.Point(158, 21);
             this.install.Name = "install";
-            this.install.Size = new System.Drawing.Size(116, 40);
+            this.install.Size = new System.Drawing.Size(116, 38);
             this.install.TabIndex = 8;
             this.install.Text = "Install as sysapp";
             this.install.UseVisualStyleBackColor = true;
@@ -188,7 +190,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label4.Location = new System.Drawing.Point(112, 57);
+            this.label4.Location = new System.Drawing.Point(118, 56);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(43, 19);
             this.label4.TabIndex = 10;
@@ -198,7 +200,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label3.Location = new System.Drawing.Point(18, 57);
+            this.label3.Location = new System.Drawing.Point(18, 56);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(88, 19);
             this.label3.TabIndex = 11;
@@ -206,9 +208,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(530, 57);
+            this.button2.Location = new System.Drawing.Point(493, 57);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 23);
+            this.button2.Size = new System.Drawing.Size(124, 23);
             this.button2.TabIndex = 12;
             this.button2.Text = "Connect through TCP";
             this.button2.UseVisualStyleBackColor = true;
@@ -216,26 +218,29 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(530, 98);
+            this.button3.Location = new System.Drawing.Point(493, 98);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(145, 23);
+            this.button3.Size = new System.Drawing.Size(125, 23);
             this.button3.TabIndex = 13;
-            this.button3.Text = "Disconect selected device";
+            this.button3.Text = "Disconect device";
+            this.ToolTip.SetToolTip(this.button3, "Disconects selected device");
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listBox1.ForeColor = System.Drawing.Color.Red;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(11, 472);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(659, 134);
+            this.listBox1.Size = new System.Drawing.Size(607, 134);
             this.listBox1.TabIndex = 14;
             this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(536, 437);
+            this.button6.Location = new System.Drawing.Point(483, 437);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(134, 23);
             this.button6.TabIndex = 15;
@@ -245,7 +250,7 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(396, 437);
+            this.button7.Location = new System.Drawing.Point(336, 437);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(134, 23);
             this.button7.TabIndex = 16;
@@ -267,8 +272,9 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.BackColor = System.Drawing.Color.Ivory;
             this.groupBox2.Controls.Add(this.button8);
-            this.groupBox2.Location = new System.Drawing.Point(256, 309);
+            this.groupBox2.Location = new System.Drawing.Point(429, 297);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(189, 115);
             this.groupBox2.TabIndex = 19;
@@ -277,13 +283,14 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.BackColor = System.Drawing.Color.Honeydew;
             this.groupBox3.Controls.Add(this.button16);
             this.groupBox3.Controls.Add(this.button11);
             this.groupBox3.Controls.Add(this.button9);
             this.groupBox3.Controls.Add(this.button10);
-            this.groupBox3.Location = new System.Drawing.Point(16, 244);
+            this.groupBox3.Location = new System.Drawing.Point(22, 297);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(233, 180);
+            this.groupBox3.Size = new System.Drawing.Size(233, 115);
             this.groupBox3.TabIndex = 20;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Platform";
@@ -291,7 +298,7 @@
             // button16
             // 
             this.button16.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button16.Location = new System.Drawing.Point(118, 26);
+            this.button16.Location = new System.Drawing.Point(122, 19);
             this.button16.Name = "button16";
             this.button16.Size = new System.Drawing.Size(102, 40);
             this.button16.TabIndex = 21;
@@ -303,9 +310,9 @@
             // button11
             // 
             this.button11.Font = new System.Drawing.Font("Arial", 8F);
-            this.button11.Location = new System.Drawing.Point(6, 114);
+            this.button11.Location = new System.Drawing.Point(122, 65);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(102, 39);
+            this.button11.Size = new System.Drawing.Size(102, 40);
             this.button11.TabIndex = 20;
             this.button11.Text = "Single Core Mode";
             this.ToolTip.SetToolTip(this.button11, "Change XMM6321 to single core mode for improved stability.\r\nLasts until next rebo" +
@@ -316,7 +323,7 @@
             // button9
             // 
             this.button9.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button9.Location = new System.Drawing.Point(6, 26);
+            this.button9.Location = new System.Drawing.Point(10, 19);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(102, 40);
             this.button9.TabIndex = 19;
@@ -327,9 +334,9 @@
             // button10
             // 
             this.button10.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button10.Location = new System.Drawing.Point(6, 72);
+            this.button10.Location = new System.Drawing.Point(10, 65);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(102, 36);
+            this.button10.Size = new System.Drawing.Size(102, 40);
             this.button10.TabIndex = 19;
             this.button10.Text = "Wake Screen";
             this.ToolTip.SetToolTip(this.button10, "Send keyevent 26 to the device");
@@ -339,9 +346,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::APK_Manager.Properties.Resources.cellphone;
-            this.pictureBox1.Location = new System.Drawing.Point(506, 190);
+            this.pictureBox1.Location = new System.Drawing.Point(286, 297);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(108, 135);
+            this.pictureBox1.Size = new System.Drawing.Size(106, 115);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
@@ -354,7 +361,8 @@
             this.button13.Size = new System.Drawing.Size(76, 40);
             this.button13.TabIndex = 11;
             this.button13.Text = "Mount r/w";
-            this.ToolTip.SetToolTip(this.button13, "File system must be mounted as read/write before files can be pushed");
+            this.ToolTip.SetToolTip(this.button13, "File system must be mounted as read/write before files can be pushed\r\nSkip if alr" +
+                    "eady mounted. Anyway, can\'t hurt.");
             this.button13.UseVisualStyleBackColor = true;
             this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
@@ -362,7 +370,7 @@
             // 
             this.button14.Enabled = false;
             this.button14.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button14.Location = new System.Drawing.Point(10, 117);
+            this.button14.Location = new System.Drawing.Point(414, 19);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(76, 40);
             this.button14.TabIndex = 12;
@@ -375,7 +383,7 @@
             // 
             this.button15.Enabled = false;
             this.button15.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.button15.Location = new System.Drawing.Point(103, 117);
+            this.button15.Location = new System.Drawing.Point(508, 19);
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(76, 40);
             this.button15.TabIndex = 13;
@@ -404,15 +412,17 @@
             // 
             // groupBox4
             // 
+            this.groupBox4.BackColor = System.Drawing.Color.SeaShell;
             this.groupBox4.Controls.Add(this.button15);
             this.groupBox4.Controls.Add(this.button14);
             this.groupBox4.Controls.Add(this.button13);
             this.groupBox4.Controls.Add(this.comboBox1);
             this.groupBox4.Controls.Add(this.label5);
             this.groupBox4.Controls.Add(this.button12);
-            this.groupBox4.Location = new System.Drawing.Point(256, 127);
+            this.groupBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox4.Location = new System.Drawing.Point(21, 202);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(189, 176);
+            this.groupBox4.Size = new System.Drawing.Size(596, 76);
             this.groupBox4.TabIndex = 21;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Push file";
@@ -426,15 +436,15 @@
             "/system/bin",
             "/sysem/data",
             "/system/lib"});
-            this.comboBox1.Location = new System.Drawing.Point(10, 80);
+            this.comboBox1.Location = new System.Drawing.Point(230, 38);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.Size = new System.Drawing.Size(117, 21);
             this.comboBox1.TabIndex = 10;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(7, 63);
+            this.label5.Location = new System.Drawing.Point(227, 19);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(120, 13);
             this.label5.TabIndex = 9;
@@ -462,7 +472,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 640);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(639, 640);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
